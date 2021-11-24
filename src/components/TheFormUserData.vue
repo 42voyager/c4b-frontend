945a792bf06f5e8bec5efe0a6260535b456f8427
MarcusVinix <vi.ni159marcus@gmail.com>
2021-11-24 10:24:48 -0300

    Adicionando textos dos termos em um array

--- src/components/TheFormUserData.vue
+++ src/components/TheFormUserData.vue
@@ -99,65 +99,8 @@
           <h1>Autorização</h1>
         </div>
         <div class="content">
-          <p>
-            Atendendo ao disposto na Resolução CMN nº 4571/2017 (“Resolução
-            SCR”), e, no artigo 1º da Resolução CMN nº 3920/2010, conforme
-            alteradas, as empresas abaixo elencadas e representadas (“Empresas”)
-            autorizam, em caráter irrevogável e irretratável o Banco ABC Brasil
-            S.A, bem como às demais empresas do Grupo ABC e seus eventuais
-            sucessores, a consultar as informações relativas a estas ou às
-            operações de crédito e operações no mercado de câmbio que estas
-            realizem ou venham a realizar, conforme disponibilizadas pelo SCR –
-            Sistema de Informações de Créditos do Banco Central do Brasil e pelo
-            SISBACEN – Sistema de Informações do Banco Central do Brasil, ou por
-            qualquer outro sistema que venha a sucedê-los.
-          </p>
-
-          <p>
-            As Empresas autorizam ainda, em caráter irrevogável e irretratável,
-            que referidas informações, no que tange a operações por estas
-            realizadas junto ao Banco ABC Brasil S.A., sejam por este, bem como
-            por demais empresas do Grupo ABC e seus eventuais sucessores,
-            registradas junto a referidos sistemas.
-          </p>
-          <p>
-            A finalidade e o uso de tais informações estão relacionados à
-            operações de crédito, nos termos do art.3º da Resolução SCR, tendo
-            como finalidades específicas: (i) o fornecimento de informações ao
-            Banco Central do Brasil para fins de monitoramento do crédito no
-            sistema financeiro e para o exercício de suas atividades de
-            fiscalização; e (ii) propiciar o intercâmbio, entre as instituições
-            financeiras sujeitas ao dever de conservar o sigilo bancário de que
-            trata a Lei Complementar nº 105/2001, das informações referentes a
-            débitos e responsabilidades de seus clientes em quaisquer operações
-            de crédito, com o objetivo de subsidiar decisões negociais e de
-            crédito.
-          </p>
-
-          <p>
-            As Empresas declaram-se cientes de que poderão consultar as
-            informações do SCR por meio do site do Registrato – Extrato do
-            Registro de Informações no BACEN ou na Central de Atendimento ao
-            Público do BACEN.
-          </p>
-          <p>
-            As eventuais manifestações de discordância quanto às informações
-            constantes do SCR e os pedidos de correções, exclusões e registros
-            de medidas judiciais no SCR deverão ser dirigidos ao Banco ABC
-            Brasil S.A, por meio de requerimento escrito e devidamente assinado
-            pelo(s) representante(s) das Empresas, conforme o caso, acompanhado
-            da respectiva decisão judicial, quando aplicável, observado o
-            disposto nos artigos 11 e 12 da Circular BACEN nº 3.870, de 19 de
-            dezembro de 2017.
-          </p>
-
-          <p>
-            Ainda, as Empresas desde já concordam expressamente em estender a
-            presente autorização de consulta ao SCR às demais instituições
-            autorizadas a consulta-lo, nos termos da Resolução SCR, que venham a
-            adquirir ou receber em garantia, ou que manifestem interesse em
-            adquirir ou receber em garantia, total ou parcialmente, a presente
-            operação de crédito.
+          <p v-for="(term, index) of terms" :key="index">
+            {{ term.text }}
           </p>
         </div>
       </div>
@@ -170,6 +113,7 @@ import { Options, Vue } from "vue-class-component";
 import ButtonDefault from "./ButtonDefault.vue";
 import InputError from "./InputError.vue";
 import Modal from "./Modal.vue";
+import { Terms } from "../text/variables";
 
 @Options({
   props: {
@@ -186,7 +130,7 @@ import Modal from "./Modal.vue";
 })
 export default class TheFormUserData extends Vue {
   openedModal = true;
-
+  terms = Terms;
   user = {
     name: "",
     email: "",
@@ -324,5 +268,6 @@ export default class TheFormUserData extends Vue {
 }
 .modal-body {
 	overflow-y: auto;
+  height: 100%;
 }
 </style>
