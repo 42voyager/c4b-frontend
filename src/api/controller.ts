374109bf20ae467f05e4e6005705709849ee1b0f
MarcusVinix <vi.ni159marcus@gmail.com>
2021-12-03 10:34:44 -0300

    começando validação do form

--- src/api/controller.ts
+++ src/api/controller.ts
@@ -1,11 +1,17 @@
 import IUserData from "@/types/user"
+import IUserFeedBack from "@/types/userFeedBack"
 import axios from 'axios'
 
-const API_URL = '/Customer/'
+const API_URL_USER = '/Customer/'
+const API_URL_FEEDBACK = '/Feedback/'
 
 export default class ApiController {
     postUser(user: IUserData): Promise<any> {
         console.log(user.name, user.email)
-        return axios.post(API_URL, user)
+        return axios.post(API_URL_USER, user)
+    }
+
+    postUserFeedback(feedback: IUserFeedBack): Promise<any> {
+        return axios.post(API_URL_FEEDBACK, feedback);
     }
 }
--- src/pages/LandingPage/TheFormFeedback.vue
+++ src/pages/LandingPage/TheFormFeedback.vue
@@ -7,30 +7,45 @@
 				type="text"
 				name="nome"
 				placeholder="Nome Completo"
+				required
 				class="input-feedback"
+				v-bind:class="{ invalid: vName, invalid: !validInput(messageResponse.Name)}"
 				v-model="userFeedBack.name"
 				/>
+			</div>
+			<div v-show="vName | !validInput(messageResponse.Name)">
+				<InputError :msg="Errors[0]"/>
 			</div>
 			<div class="wrapper-input">
 				<input
 				type="email"
 				name="email"
 				placeholder="E-mail"
+				required
 				class="input-feedback"
+				v-bind:class="{ invalid: vEmail, invalid: !validInput(messageResponse.Email)}"
 				v-model="userFeedBack.email"
 				/>
+			</div>
+			<div v-show="vEmail | !validInput(messageResponse.Email)">
+				<InputError :msg="Errors[1]"/>
 			</div>
 			<div class="wrapper-input">
 				<label for="feedback" class="label-message">feedback/dúvida</label>
 				<textarea 
 				name="feedback" 
+				placeholder="Mensagem"
 				id="feedback"
 				class="textarea-input"
+				v-bind:class="{ invalid: vMessage, invalid: !validInput(messageResponse.Message) }"
 				cols="40" 
 				rows="10" 
 				required
 				v-model="userFeedBack.message"
 				/>
+			</div>
+			<div v-show="vMessage | !validInput(messageResponse.Message)">
+				<InputError :msg="Errors[2]"/>
 			</div>
 			<ButtonDefault @buttonClicked="submitFeedBack()"/>
 		</form>
@@ -39,33 +54,76 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import ApiController from "@/api/controller";
 import ButtonDefault from "@/components/ui/ButtonDefault.vue";
+import InputError from "@/components/ui/InputError.vue";
 import IUserFeedBack from "@/types/userFeedBack"
 
 @Options({
-	emits: ["submitFeedback"],
 	components: {
-		ButtonDefault
+		ButtonDefault,
+		InputError
 	}})
 export default class TheFormFeedback extends Vue {
+	vName = false;
+	vEmail = false;
+	vMessage = false;
+	Errors = [["Wrong Name"], ["Wrong Email"], ["Wrong Message"]]
+	messageResponse = []
 	userFeedBack: IUserFeedBack = {
 				name: "",
 				email: "",
 				message: ""
 			};
-		submitFeedBack(): void {
-			this.$emit("submitFeedback", this.userFeedBack)
-			console.log("DADOS", this.userFeedBack)
-			this.resetFeedBack()
-		}
-		resetFeedBack(): void {
-			const emptyUserFeedBack: IUserFeedBack = {
-				name: "",
-				email: "",
-				message: ""
-			};
-			this.userFeedBack = emptyUserFeedBack 
-		}
+	submitFeedBack(): void {
+		//console.log("DADOS", this.userFeedBack)
+		if (!this.validForm(this.userFeedBack))
+			return
+		new ApiController()
+			.postUserFeedback(this.userFeedBack)
+			.then((res) => {
+				console.log(res);
+				this.resetFeedBack()
+				this.vName = false;
+				this.vEmail = false;
+				this.vMessage = false;
+			})
+			.catch((err) => {
+				console.log("Error:  ", err)
+				this.messageResponse = err.response.data.errors
+			})
+	}
+	resetFeedBack(): void {
+		const emptyUserFeedBack: IUserFeedBack = {
+			name: "",
+			email: "",
+			message: ""
+		};
+		this.userFeedBack = emptyUserFeedBack 
+	}
+	validForm(userFeedBack: IUserFeedBack): boolean
+	{
+		if (userFeedBack.name.length < 2)
+			this.vName = true;
+		else
+			this.vName = false;
+		if (userFeedBack.email.length < 11)
+			this.vEmail = true;
+		else
+			this.vEmail = false;
+		if (userFeedBack.message.length < 5)
+			this.vMessage = true;
+		else
+			this.vMessage = false;
+		if (this.vName || this.vEmail || this.vMessage)
+			return (false)
+		else
+			return (true)
+	}
+	validInput(data: Array<string>): boolean {
+		if (data != undefined && data.length != 0) return false;
+		else return true;
+	}
 }
 </script>
 
@@ -97,5 +155,9 @@ export default class TheFormFeedback extends Vue {
 }
 .wrapper-input {
 	text-align: left;
+}
+.invalid::placeholder {
+	color: red;
+	opacity: 1;
 }
 </style>
