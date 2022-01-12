a93bc0e7d7f9fa503754104dd7933ffc99ed8e4a
Diego <diegocl02@gmail.com>
2022-01-12 10:54:16 -0500

    melhoras no dropdown do bank info

--- src/components/ui/InputError.vue
+++ src/components/ui/InputError.vue
@@ -1,32 +1,32 @@
 <template>
-	<p>{{ msg.join(" ") }}</p>
+    <p>{{ msg.join(' ') }}</p>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType } from 'vue'
 
 /**
  * Component utilizado para mostrar os erros dos inputs
  * @prop {Array<String>} msg - Um array de strings contendo todas as mensagens de erro
  */
 export default defineComponent({
-	props: {
-			msg: {
-				type: Array as PropType<string[]>,
-				required: true
-			}
-		}
-});
+    props: {
+        msg: {
+            type: Array as PropType<string[]>,
+            required: true,
+        },
+    },
+})
 </script>
 
 <style scoped>
 p {
-	font-size: 12px;
-	color: red;
-	padding: 5px;
-	max-width: 300px;
-	width: 100%;
-	margin-top: 0;
-	text-align: left;
+    font-size: 12px;
+    color: red;
+    padding: 5px;
+    max-width: 300px;
+    width: 100%;
+    margin-top: 0;
+    text-align: left;
 }
 </style>
--- src/components/ui/MultiSelect.vue
+++ src/components/ui/MultiSelect.vue
@@ -1,35 +1,51 @@
 <template>
-	<VueMultiselect
-		class="default"
-		v-bind="$attrs"
-		v-model="value"
-		:options="options"
-		:showLabels="false">
-	</VueMultiselect>
+    <VueMultiselect
+        class="default"
+        v-bind="$attrs"
+        v-model="value"
+        :options="options"
+        :showLabels="false"
+    >
+    </VueMultiselect>
+    <div v-show="isInvalid">
+        <InputError :msg="errors" />
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, PropType } from 'vue'
+import InputError from '@/components/ui/InputError.vue'
 import VueMultiselect from 'vue-multiselect'
 
 export default defineComponent({
-	components: {
-		VueMultiselect
-	},
-	props: {
-		options: {
-			type: Array,
-			required: true
-		}
-	},
-	inheritAttrs: false,
-	setup() {
-		const value = ref('')
-		
-		return {
-			value
-		}
-	},
+    components: {
+        VueMultiselect,
+        InputError,
+    },
+    props: {
+        isInvalid: {
+            type: Boolean,
+            default: false,
+        },
+        errors: {
+            type: Array as PropType<string[]>,
+            default: () => {
+                return ['']
+            },
+        },
+        options: {
+            type: Array,
+            required: true,
+        },
+    },
+    inheritAttrs: false,
+    setup() {
+        const value = ref('')
+
+        return {
+            value,
+        }
+    },
 })
 </script>
 
@@ -37,28 +53,46 @@ export default defineComponent({
 
 <style scoped>
 .default {
-	max-width: 100%;
-	font-size: 18px;
-	margin: 10px 0 !important;
+    max-width: 100%;
+    font-size: 18px;
+    margin: 10px 0 !important;
+}
+:deep .multiselect__content-wrapper::-webkit-scrollbar {
+    width: 12px;
+}
+:deep .multiselect__content-wrapper::-webkit-scrollbar-thumb {
+    background: #ddb893;
+    border-radius: 10px;
+    height: 30px;
+}
+:deep .multiselect__content-wrapper::-webkit-scrollbar-thumb:hover {
+    background: #c09f7f;
+}
+:deep .multiselect__content-wrapper::-webkit-scrollbar-track {
+    background: #fcf6f0;
+}
+:deep .multiselect__option {
+    white-space: normal;
+    line-height: 22px;
 }
 :deep .multiselect__placeholder {
-	font-size: 18px !important;
-	color: rgb(116, 114, 114) !important;
-	padding: 0;
-	margin-bottom: 0px;
+    font-size: 18px !important;
+    color: rgb(116, 114, 114) !important;
+    padding: 0;
+    margin-bottom: 0px;
 }
 :deep .multiselect__single {
-	font-size: 18px !important;
-	color: black !important;
-	padding: 0;
-	margin-bottom: 0;
+    font-size: 18px !important;
+    color: black !important;
+    padding: 0;
+    margin-bottom: 0;
 }
 :deep .multiselect__tags {
-	padding: 14px !important;
-	border-radius: 5px;
-	border: solid 1px #b29475;
+    padding: 14px !important;
+    border-radius: 5px;
+    border: solid 1px #b29475;
 }
 :deep .multiselect__select {
-	top: 10px;
+    top: 10px;
 }
 </style>
