d306e0a53e9b755813ff27f979ac7d29c69619f4
Microsoft Learn Student <learn@contoso.com>
2022-01-20 13:24:42 -0300

    slider with two color background

--- src/components/ui/SliderInput.vue
+++ src/components/ui/SliderInput.vue
@@ -1,17 +1,17 @@
 <template>
-	<div class="slider-wrapper">
-		<input
-			class="slider-input"
-			v-bind="$attrs"
-			type="range"
-			:value="modelValue"
-			@input="$emit('update:modelValue', Number($event.target.value))"
-		/>
-		<div class="min-max-wrapper" v-show="showLabels">
-			<p> {{ minLabel }} </p>
-			<p> {{ maxLabel }} </p>
-		</div>
-	</div>
+    <div class="slider-wrapper">
+        <input
+            class="slider-input"
+            v-bind="$attrs"
+            type="range"
+            :value="modelValue"
+            @input="$emit('update:modelValue', Number($event.target.value))"
+        />
+        <div class="min-max-wrapper" v-show="showLabels">
+            <p>{{ minLabel }}</p>
+            <p>{{ maxLabel }}</p>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -25,72 +25,94 @@ import { defineComponent } from 'vue'
  * @prop {string} maxLabel - label para indicar o valor maximo
  */
 export default defineComponent({
-	props: {
-		showLabels: {
-			type: Boolean,
-			default: false
-		},
-		modelValue: {
-			type: Number,
-			required: true
-		},
-		minLabel: {
-			type: String,
-			required: true
-		},
-		maxLabel: {
-			type: String,
-			required: true
-		}
-	},
-	inheritAttrs: false
+    props: {
+        showLabels: {
+            type: Boolean,
+            default: false,
+        },
+        modelValue: {
+            type: Number,
+            required: true,
+        },
+        minLabel: {
+            type: String,
+            required: true,
+        },
+        maxLabel: {
+            type: String,
+            required: true,
+        },
+    },
+    inheritAttrs: false,
 })
 </script>
 
 <style scoped>
 .slider-wrapper {
-	display: flex;
-	justify-content: center;
-	padding: 15px 0px;
-	width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 15px 0px;
+    width: 100%;
 }
 .slider-input {
-	-webkit-appearance: none;
-	height: 10px;
-	background: #fff;
-	border-radius: 5px;
-	outline: none;
-	opacity: 0.7;
-	-webkit-transition: .2s;
-	transition: opacity .2s;
-	width: calc(100% - 30px);
+    -webkit-appearance: none;
+    height: 25px;
+    background: #b9ada0;
+    border-radius: 25px;
+    outline: none;
+    opacity: 0.7;
+	overflow: hidden;
+    -webkit-transition: 0.2s;
+    transition: opacity 0.2s;
+    width: calc(80% - 30px);
 }
 .slider-input:hover {
-  opacity: 1;
+    opacity: 1;
 }
 
 .slider-input::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: #b29475;
-  cursor: pointer;
-  border: none;
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: #64380c;
+	box-shadow: -500px 0 0 490px #807264;
+	cursor: pointer;
+    border: none;
 }
 
 .slider-input::-moz-range-thumb {
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: #b29475;
-  cursor: pointer;
-  border: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: #64380c;
+    cursor: pointer;
+    border: none;
 }
 .min-max-wrapper {
-	display: flex;
-	justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 }
-
+.slider-input::-webkit-slider-runnable-track {
+    color: #b9ada0;
+	height: 25px;
+}
+.slider-input::-moz-range-progress {
+    background-color: #807264;
+	height: 25px;
+}
+.slider-input::-moz-range-track {
+    background-color: #b9ada0;
+	height: 25px;
+}
+/* IE*/
+.slider-input::-ms-fill-lower {
+    background-color: #807264;
+	height: 25px;
+}
+.slider-input::-ms-fill-upper {
+    background-color: #b9ada0;
+	height: 25px;
+}
 </style>
