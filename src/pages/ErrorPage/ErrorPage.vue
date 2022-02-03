268e73cc542fb6c8f45b4e27191e60d9e23479d1
Diego <diegocl02@gmail.com>
2022-02-03 15:13:45 -0500

    adicionado pagina

---
+++ src/pages/ErrorPage/ErrorPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <TheHeader>
+        <template #menu-list>
+            <NavBarMenuItems
+                :listItems="ListItemsNavBarBankForm"/>
+        </template>
+    </TheHeader>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TheHeader from '@/components/layout/TheHeader.vue'
+import { ListItemsNavBarBankForm } from '@/config/variables'
+
+export default defineComponent({
+	components: {
+		TheHeader
+	},
+	setup() {
+		return {
+			ListItemsNavBarBankForm
+		}		
+	},
+})
+</script>
+
+<style scoped>
+
+</style>
